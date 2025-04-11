72cb35812fc1ed8c0c336a7ff3951d9cc713c748
Zidd <nuvyazid@gmail.com>
2025-04-11 13:53:51 +0700

    add SSLTrust.kt

---
+++ core/src/main/java/com/nuvyz/core/data/remote/SSLTrust.kt
@@ -0,0 +1,153 @@
+package com.nuvyz.core.data.remote
+
+import android.content.Context
+import coil.ImageLoader
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import java.io.IOException
+import java.io.InputStream
+import java.security.GeneralSecurityException
+import java.security.KeyStore
+import java.security.SecureRandom
+import java.security.cert.Certificate
+import java.security.cert.CertificateException
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import java.util.*
+import javax.net.ssl.*
+
+/*
+ * Copyright (C) 2015 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class SSLTrust {
+    /**
+     * Returns a trust manager that trusts `certificates` and none other. HTTPS services whose
+     * certificates have not been signed by these certificates will fail with a `SSLHandshakeException`.
+     *
+     *
+     * This can be used to replace the host platform's built-in trusted certificates with a custom
+     * set. This is useful in development where certificate authority-trusted certificates aren't
+     * available. Or in production, to avoid reliance on third-party certificate authorities.
+     *
+     *
+     * See also [CertificatePinner], which can limit trusted certificates while still using
+     * the host platform's built-in trust store.
+     *
+     * <h3>Warning: Customizing Trusted Certificates is Dangerous!</h3>
+     *
+     *
+     * Relying on your own trusted certificates limits your server team's ability to update their
+     * TLS certificates. By installing a specific set of trusted certificates, you take on additional
+     * operational complexity and limit your ability to migrate between certificate authorities. Do
+     * not use custom trusted certificates in production without the blessing of your server's TLS
+     * administrator.
+     */
+    @Throws(GeneralSecurityException::class)
+    fun trustManagerForCertificates(`in`: InputStream?): X509TrustManager {
+        val certificateFactory = CertificateFactory.getInstance("X.509")
+        val certificates = certificateFactory.generateCertificates(`in`)
+        if (certificates.isEmpty()) {
+            throw IllegalArgumentException("expected non-empty set of trusted certificates")
+        }
+
+        // Put the certificates a key store.
+        val password = "password".toCharArray() // Any password will work.
+        val keyStore = newEmptyKeyStore(password)
+        for ((index, certificate: Certificate?) in certificates.withIndex()) {
+            val certificateAlias = index.toString()
+            keyStore.setCertificateEntry(certificateAlias, certificate)
+        }
+
+        // Use it to build an X509 trust manager.
+        val keyManagerFactory = KeyManagerFactory.getInstance(
+            KeyManagerFactory.getDefaultAlgorithm()
+        )
+        keyManagerFactory.init(keyStore, password)
+        val trustManagerFactory = TrustManagerFactory.getInstance(
+            TrustManagerFactory.getDefaultAlgorithm()
+        )
+        trustManagerFactory.init(keyStore)
+        val trustManagers = trustManagerFactory.trustManagers
+        if (trustManagers.size != 1 || trustManagers[0] !is X509TrustManager) {
+            throw IllegalStateException(
+                "Unexpected default trust managers:"
+                        + Arrays.toString(trustManagers)
+            )
+        }
+        return trustManagers[0] as X509TrustManager
+    }
+
+    @Throws(GeneralSecurityException::class)
+    private fun newEmptyKeyStore(password: CharArray): KeyStore {
+        try {
+            val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+            val `in`: InputStream? = null // By convention, 'null' creates an empty key store.
+            keyStore.load(`in`, password)
+            return keyStore
+        } catch (e: IOException) {
+            throw AssertionError(e)
+        }
+    }
+
+    fun createUnsafeTrustManager(): X509TrustManager {
+        return object : X509TrustManager {
+            @Throws(CertificateException::class)
+            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+            }
+
+            @Throws(CertificateException::class)
+            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate?> {
+                return arrayOf()
+            }
+        }
+    }
+
+    companion object {
+        fun createUnsafeOkHttpClient(): OkHttpClient.Builder {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return arrayOf()
+                }
+            })
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL").apply {
+                init(null, trustAllCerts, SecureRandom())
+            }
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.socketFactory
+
+            return OkHttpClient.Builder()
+                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                .hostnameVerifier { _, _ -> true }
+        }
+
+        fun createUnsafeTrustManagerImageLoader(context: Context): ImageLoader {
+            return ImageLoader.Builder(context)
+                .okHttpClient(createUnsafeOkHttpClient().build())
+                .build()
+        }
+    }
+}
